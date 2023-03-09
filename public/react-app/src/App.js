89e413781496203f00c8c61436c00e9313f67725
rekhan <rekhan@expediagroup.com>
2023-03-09 11:01:45 +0000

    created frontend view for all items and created react routers

--- public/react-app/src/App.js
+++ public/react-app/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import ItemList from "./components/ItemList";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Routes>
+        <Route path="/items" element={<ItemList />}></Route>
+      </Routes>
     </>
   );
 }
---
+++ public/react-app/src/components/ItemList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Item from "./Item";
+import { v4 } from "uuid";
+export default function ItemList() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await fetch("http://localhost:3000/items");
+
+      const data = await res.json();
+
+      setItems(data);
+    };
+    fetchItems();
+  }, []);
+  console.log(items, "items");
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item key={v4()} item={item} />
+      ))}
+    </ul>
+  );
+}
