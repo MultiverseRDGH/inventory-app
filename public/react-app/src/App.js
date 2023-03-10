1166fe60350c1a61eae203e9cd887605abc65049
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-03-10 11:42:06 +0000

    Changed index route to render items

--- public/react-app/src/App.js
+++ public/react-app/src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import ItemList from "./components/ItemList";
-import { Routes, Route } from "react-router-dom";
-import SingleItemPage from "./pages/SingleItemPage";
-import NewItemPage from "./pages/NewItemPage";
-import { createContext, useState } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import './App.css';
+import ItemList from './components/ItemList';
+import { Routes, Route } from 'react-router-dom';
+import SingleItemPage from './pages/SingleItemPage';
+import NewItemPage from './pages/NewItemPage';
+import { createContext, useState } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-import OrdersPage from "./pages/OrdersPage";
+import OrdersPage from './pages/OrdersPage';
 
 export const OrderContext = createContext({
   orders: [],
@@ -22,7 +22,7 @@ function App() {
       <OrderContext.Provider value={{ orders, setOrders }}>
         <Navbar />
         <Routes>
-          <Route path="/items" element={<ItemList />}></Route>
+          <Route path="/" element={<ItemList />}></Route>
           <Route path="/items/orders" element={<OrdersPage />}></Route>
           <Route path="/items/single/:id" element={<SingleItemPage />}></Route>
           <Route path="/items/new" element={<NewItemPage />}></Route>
