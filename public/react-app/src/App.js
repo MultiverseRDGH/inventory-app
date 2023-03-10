480587105b51829fc0ae1c9c1fdc81e3efc55598
rekhan <rekhan@expediagroup.com>
2023-03-10 10:27:30 +0000

    Navigation for user using the navbar

--- public/react-app/src/App.js
+++ public/react-app/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import ItemList from './components/ItemList';
-import { Routes, Route } from 'react-router-dom';
-import SingleItemPage from './pages/SingleItemPage';
-import NewItemPage from './pages/NewItemPage';
-import { createContext, useEffect, useState } from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import ItemList from "./components/ItemList";
+import { Routes, Route } from "react-router-dom";
+import SingleItemPage from "./pages/SingleItemPage";
+import NewItemPage from "./pages/NewItemPage";
+import { createContext, useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import OrdersPage from './pages/OrdersPage';
+import OrdersPage from "./pages/OrdersPage";
 
 export const OrderContext = createContext({
   orders: [],
@@ -21,9 +21,9 @@ function App() {
     <>
       <OrderContext.Provider value={{ orders, setOrders }}>
         <Navbar />
-        <OrdersPage />
         <Routes>
           <Route path="/items" element={<ItemList />}></Route>
+          <Route path="/items/orders" element={<OrdersPage />}></Route>
           <Route path="/items/single/:id" element={<SingleItemPage />}></Route>
           <Route path="/items/new" element={<NewItemPage />}></Route>
         </Routes>
